78795409bc3b1f9cbb4c54ed5ce7ba04612b9e0d
Cristiano Betta <cbetta@paypal.com>
2013-07-17 14:11:02 +0100

    Fixes auth on tweetscontroller

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,7 @@
 class TweetsController < ApplicationController
+
+  skip_before_filter :authenticate
+
   def index
     render json: Tweet.order("tweeted_at ASC").last(10), only: [:xid, :text, :name, :screen_name, :media_url, :profile_image_url, :tweeted_at]
   end
